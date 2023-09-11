5c4bc0b4cfc0d934e0a3b5ccfa98960ded8b36a3
Igor Reus <igorsreus@gmail.com>
2023-09-11 11:50:50 +0300

    Rev.3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int rub = 14000; // количество денег
+        int count = 153; // количество денег на счету
+        int rub = 14000; // количество денег при пополнении
         int percent = (1 * 100 / 100); // формула вычисления процентов
         int bonus; // переменная бонусов
         if (rub > 100) {
             bonus = percent * rub / 100; // условие при платеже более 100 руб.
         } else {
-            bonus = 0; // условие при платеже менее 100 руб.
+            bonus = rub * 0; // условие при платеже менее 100 руб.
         }
 
-        System.out.println("Количество бонусов составит " + bonus + " рубля(ей)");
+        System.out.println("Сумма пополнения составила " + (rub + bonus) + " рубля(ей)");
+        System.out.println("Из них бонусов: " + bonus + " рубля(ей)");
+        System.out.println("Итого на Вашем счету: " + (count + rub + bonus));
     }
 }
